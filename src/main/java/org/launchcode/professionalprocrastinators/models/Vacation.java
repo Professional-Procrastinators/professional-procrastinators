2f5fac4f026089621160120935cc5ff4dfd49609
etc1234567 <shootingstaretc@gmail.com>
2023-12-16 17:41:40 -0600

    Added Vacation model and add vacation page with form.

---
+++ src/main/java/org/launchcode/professionalprocrastinators/models/Vacation.java
@@ -0,0 +1,53 @@
+package main.java.org.launchcode.professionalprocrastinators.models;
+
+
+import org.jetbrains.annotations.NotNull;
+
+public class Vacation {
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String country;
+
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Vacation(String city, String country, String state) {
+        this.city = city;
+        this.country=country;
+        this.state= state;
+    }
+
+    @Override
+    public String toString() {
+        return "Vacation{" +
+                "city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
