c429267d9b82dc267e29c7f740e884bcc2bb9b99
Pell35 <pjelliott11@gmail.com>
2023-12-18 15:45:22 -0600

    Added more to ProfileController, and created a TODO

--- src/main/java/org/launchcode/professionalprocrastinators/controllers/ProfileController.java
+++ src/main/java/org/launchcode/professionalprocrastinators/controllers/ProfileController.java
@@ -1,4 +1,21 @@
 package org.launchcode.professionalprocrastinators.controllers;
 
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/profile")
 public class ProfileController {
+
+    @GetMapping
+    public String viewProfile(@ModelAttribute Model model) {
+
+        model.addAttribute("user", user);
+        return "profile";
+    }
 }
+// TODO: Create a way to fetch user data to use in viewProfile Method
