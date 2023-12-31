134b4976f17b1e46e8ce988a8eef4c409301ba44
jimiller20 <jmiller20twenty@gmail.com>
2023-12-31 16:37:16 -0600

    added user login page and create account page in the works.

--- src/main/java/org/launchcode/professionalprocrastinators/controllers/UserController.java
+++ src/main/java/org/launchcode/professionalprocrastinators/controllers/UserController.java
@@ -1,7 +1,27 @@
 package org.launchcode.professionalprocrastinators.controllers;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+
+    @GetMapping(value = "/login")
+    public String loginProcessForm(Model model) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(Model model) {
+        return ":redirect";
+    }
+
+    @GetMapping(value = "/login/create_account")
+    public String createAccountProcessFrom(Model model) {
+        return "create-account";
+    }
+
 }
