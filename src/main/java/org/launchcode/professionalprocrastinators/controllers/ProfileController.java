680d27414013863371a90cd8920fa6fb823376cb
Pj Elliott <pjelliott11@gmail.com>
2024-01-01 11:46:01 -0600

    Updated name and url in application.properties and in SQL.

--- src/main/java/org/launchcode/professionalprocrastinators/controllers/ProfileController.java
+++ src/main/java/org/launchcode/professionalprocrastinators/controllers/ProfileController.java
@@ -2,15 +2,21 @@ package org.launchcode.professionalprocrastinators.controllers;
 
 
 import ch.qos.logback.core.model.Model;
+import org.launchcode.professionalprocrastinators.models.User;
+import org.launchcode.professionalprocrastinators.models.Vacation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/profile")
 public class ProfileController {
 
+    private static List<User> user = new ArrayList<>();
     @GetMapping
     public String viewProfile(@ModelAttribute Model model) {
 
