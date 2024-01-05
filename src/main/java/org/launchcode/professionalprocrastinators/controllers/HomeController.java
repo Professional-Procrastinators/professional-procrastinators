561415f3b4592e7bbeea29a656658186b9727ccf
Etc1234567 <138387592+Etc1234567@users.noreply.github.com>
2024-01-04 19:23:17 -0600

    Removed duplicate VacationRepository

--- src/main/java/org/launchcode/professionalprocrastinators/controllers/HomeController.java
+++ src/main/java/org/launchcode/professionalprocrastinators/controllers/HomeController.java
@@ -1,4 +1,5 @@
 package org.launchcode.professionalprocrastinators.controllers;
+import org.launchcode.professionalprocrastinators.models.data.VacationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
