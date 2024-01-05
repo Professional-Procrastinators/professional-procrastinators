2c392c911358356fdb0acff18436760b8032037e
Pj Elliott <pjelliott11@gmail.com>
2024-01-04 19:29:50 -0600

    Set up password variable with getter and setter

--- src/main/java/org/launchcode/professionalprocrastinators/models/User.java
+++ src/main/java/org/launchcode/professionalprocrastinators/models/User.java
@@ -15,6 +15,7 @@ public class User {
     @NotNull
     @Size
        private String username;
+        private String password;
        private String name;
         private String email;
         private String location;
@@ -34,6 +35,14 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getName() {
