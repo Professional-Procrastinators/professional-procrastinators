9528d523e78523272984e778dd252fa5635afb9c
Pj Elliott <pjelliott11@gmail.com>
2024-01-04 18:32:13 -0600

    Updated User Model to include variables, getters and setters and toString

--- src/main/java/org/launchcode/professionalprocrastinators/models/User.java
+++ src/main/java/org/launchcode/professionalprocrastinators/models/User.java
@@ -3,6 +3,8 @@ package org.launchcode.professionalprocrastinators.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import org.jetbrains.annotations.NotNull;
+
 
 @Entity
 public class User {
@@ -10,17 +12,67 @@ public class User {
     @GeneratedValue
     private int id;
 
-
+    @NotNull
+    @Size
        private String username;
        private String name;
         private String email;
         private String location;
         private String numOfVacations;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getNumOfVacations() {
+        return numOfVacations;
+    }
+
+    public void setNumOfVacations(String numOfVacations) {
+        this.numOfVacations = numOfVacations;
+    }
+    @Override
+    public String toString() {
+        return "Username: '" + username + "', Name: '" + name + "', Location: '" + location + "', Vacations Taken: " + numOfVacations;
+    }
 }
 
 //TODO: Set up user information variables, with getters and setters
 //TODO: How to connect SQL URL with React URL @CrossOrigin?
-// I think I've completed the CrossOrigins one, but leaving just in case
 
