cedf1567dc4cbd85911cf6ab0beb6c3f3b957275
Etc1234567 <138387592+Etc1234567@users.noreply.github.com>
2024-01-10 20:33:55 -0600

    YouTube videos can be added to the homepage!

--- src/main/java/org/launchcode/professionalprocrastinators/controllers/ProfileController.java
+++ src/main/java/org/launchcode/professionalprocrastinators/controllers/ProfileController.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
 @RequestMapping("/profile")
 public class ProfileController {
     @Autowired
--- src/main/java/org/launchcode/professionalprocrastinators/controllers/UserController.java
+++ src/main/java/org/launchcode/professionalprocrastinators/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
 
     @GetMapping(value = "/login")
