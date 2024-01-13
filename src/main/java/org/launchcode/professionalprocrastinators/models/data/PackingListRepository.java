a9bd0c2ac3e0c2fbd39115dce52b8f46dfe101b1
Pj Elliott <pjelliott11@gmail.com>
2024-01-12 23:56:13 -0600

    Created Packing List Repository

---
+++ src/main/java/org/launchcode/professionalprocrastinators/models/data/PackingListRepository.java
@@ -0,0 +1,6 @@
+package org.launchcode.professionalprocrastinators.models.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PackingListRepository extends CrudRepository {
+}
