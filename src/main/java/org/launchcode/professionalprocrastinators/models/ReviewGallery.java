a9d166ef9418d015e68beaa4202d7448248db319
John Clowers <jclowers18@gmail.com>
2024-01-17 16:57:51 -0600

    review gallery - class

--- src/main/java/org/launchcode/professionalprocrastinators/models/ReviewGallery.java
+++ src/main/java/org/launchcode/professionalprocrastinators/models/ReviewGallery.java
@@ -1,4 +1,58 @@
 package org.launchcode.professionalprocrastinators.models;
 
-public class ReviewGallery {
-}
+import jakarta.persistence.*;
+
+    @Entity
+    public class ReviewGallery {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String imageUrl;
+
+        private String description;
+
+        private int rating;
+
+        public ReviewGallery(Long id, String imageUrl, String description, int rating) {
+            this.id = id;
+            this.imageUrl = imageUrl;
+            this.description = description;
+            this.rating = rating;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public int getRating() {
+            return rating;
+        }
+
+        public void setRating(int rating) {
+            this.rating = rating;
+        }
+    }
+
+
